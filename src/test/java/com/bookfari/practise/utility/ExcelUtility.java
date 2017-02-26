2f2669dd292c700cf66f6dc592a4ca288041dcea
vidushi1991 <vidushimishra1691@gmail.com>
2017-02-26 17:15:59 +0530

    update write excel code

--- src/test/java/com/bookfari/practise/utility/ExcelUtility.java
+++ src/test/java/com/bookfari/practise/utility/ExcelUtility.java
@@ -5,6 +5,8 @@ package com.bookfari.practise.utility;
 
 import java.io.File;
 import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
@@ -12,6 +14,7 @@ import java.util.List;
 import org.apache.poi.ss.usermodel.Row;
 import org.apache.poi.ss.usermodel.Sheet;
 import org.apache.poi.ss.usermodel.Workbook;
+import org.apache.poi.xssf.usermodel.XSSFSheet;
 import org.apache.poi.xssf.usermodel.XSSFWorkbook;
 
 import com.bookfari.practise.model.Customer;
@@ -40,5 +43,27 @@ public class ExcelUtility {
 		workbook.close();
 		inputStream.close();
 		return list;
+	}
+	
+	/**
+	 * Write excel Data in new Excel File... 
+	 * @throws IOException 
+	 */
+	public void writeExcel(List<Customer> customerList) throws IOException{
+		 	XSSFWorkbook workbook = new XSSFWorkbook();
+	        XSSFSheet sheet = workbook.createSheet("Report");
+	        int rowCount = 0;
+	        Row row1 = sheet.createRow(rowCount);
+            row1.createCell(0).setCellValue("UserName");
+            row1.createCell(1).setCellValue("Password");
+            row1.createCell(2).setCellValue("Result");	 
+	        for (Customer customer : customerList) {
+	        	 Row row = sheet.createRow(++rowCount);
+	             row.createCell(0).setCellValue(customer.getUserName());
+	             row.createCell(1).setCellValue(customer.getPassword());
+	             row.createCell(2).setCellValue(customer.getResult());	             
+			}
+	        FileOutputStream outputStream = new FileOutputStream("TestReport.xlsx");
+	        workbook.write(outputStream);
 	}
 }
