f0d5b42a858871ef1dda5a1c00a06258e33bf348
vidushi1991 <vidushimishra1691@gmail.com>
2017-03-03 19:16:31 +0530

    add if loop to check condition

--- src/test/java/com/vidu/utility/LoginUtility.java
+++ src/test/java/com/vidu/utility/LoginUtility.java
@@ -10,7 +10,9 @@ import java.util.List;
 import org.openqa.selenium.By;
 import org.openqa.selenium.NoSuchElementException;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.Assert;
 
 import com.vidu.model.Customer;
 
@@ -21,6 +23,7 @@ import com.vidu.model.Customer;
 public class LoginUtility {
 
 	WebDriver driver;
+	boolean result = false;
 	public List<Customer> loginTesting() throws InterruptedException, IOException, NoSuchElementException{
 		System.setProperty("webdriver.chrome.driver", "C://Users//Vidushi Mishra//workspace//Bookfariautomation//Lib//chromedriver.exe");
 		ExcelUtility excelUtility=new ExcelUtility();
@@ -40,36 +43,48 @@ public class LoginUtility {
 			driver.findElement(By.id("UserEmail")).sendKeys(customer.getUserName());
 			driver.findElement(By.id("UserPwd")).sendKeys(customer.getPassword());
 			driver.findElement(By.id("btnLogin")).click();
-			Thread.sleep(8000);
-			try {
-				if(driver.findElement(By.id("UserEmailWrongCombinationErrorMsg")).isDisplayed()){			
+			Thread.sleep(4000);
+
+			LoginUtility checkElement = new LoginUtility();
+			checkElement.isElementFound("UserEmailWrongCombinationErrorMsg");
+			if (result = true) {
+
+
+
+				if (driver.findElement(By.id("UserEmailWrongCombinationErrorMsg")).isDisplayed())
+				{				
 					driver.findElement(By.id("UserEmail")).clear();
 					driver.findElement(By.id("UserPwd")).clear();
 					finalCustomerList.add(new Customer(customer.getUserName(),customer.getPassword(),"Not Authenticate"));
 				}
-			
-				
-				else{
-					finalCustomerList.add(new Customer(customer.getUserName(),customer.getPassword(),"Authenicate"));
-					//TODO logout functionality
-					driver.findElement(By.linkText("LOGOUT.")).click();
-					Thread.sleep(4000);
-
-				}
-				
-			
-			}catch (Exception e) {
-				// TODO: handle exception
 			}
 
-				
+			else {
+				driver.findElement(By.linkText("LOGOUT.")).click();
 
-			
-			
+				System.out.println("vidushi");
+				finalCustomerList.add(new Customer(customer.getUserName(),customer.getPassword(),"Authenicate"));
+				//TODO logout functionality
+				Thread.sleep(4000);
+			}
+
 
 		}
 		return finalCustomerList;
-}
+	}
+	public boolean isElementFound( String text) {
+
+		try{
+			WebElement webElement = driver.findElement(By.xpath(text));
+			System.out.println("isElementFound : true :"+text + "true");
+		}catch(NoSuchElementException e){
+			System.out.println("isElementFound : false :"+text);
+			result = false;
+			return false;
+		}
+		result = true;
+		return true;
+	}
 }
 
 
